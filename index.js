c24150851bc87cf390b38d91dc337cb27449040e
kolamat <matanmidupe004@gmail.com>
2024-08-23 08:56:41 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const primary = document.createElement("p")
+  primary.textContent = "Ago-Egba Nursery and Primary School"
+document.querySelector("body").appendChild(primary)
+
+const juniorSecondary = "Denton Junior Grammar School"
+  juniorSecondary.textContent = "Ago-Egba Nursery and Primary School"
+document.querySelector("body").appendChild(juniorSecondary)
+
+const SeniorSecondary = "Lagos-City Senior Secondary School"
+  SeniorSecondary.textContent = "Ago-Egba Nursery and Primary School"
+document.querySelector("body").appendChild(SeniorSecondary)
