2e680b60dccc2a0b8a7644931c8217dfd586b25e
kolamat <matanmidupe004@gmail.com>
2024-08-23 16:46:31 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   contact.html
    	modified:   contact.js

--- contact.js
+++ contact.js
@@ -10,6 +10,7 @@ contact.style.placeItems = "center";
 const orderedList = document.querySelector("ol");
 orderedList.style.gap = "10px";
 orderedList.style.display = "grid";
+orderedList.style.padding = "0px";
 
 const whatsappNumber = document.createElement("a");
 whatsappNumber.textContent = "Number : Whatsapp(2348150672809)";
@@ -17,14 +18,35 @@ whatsappNumber.href = "https://wa.me/+2348150672809";
 whatsappNumber.style.textDecoration = "none";
 document.querySelector("ol").appendChild(whatsappNumber);
 
+const hr = document.createElement("hr");
+hr.style.background = "#b06969";
+hr.style.width = "100%";
+hr.style.margin = "15px";
+hr.style.height = "10px";
+document.querySelector("ol").appendChild(hr);
+
 const email = document.createElement("a");
 email.textContent = "Email : Matanmidupe004@gmail.com";
 email.href = "https://matanmidupe004@gmail.com";
 email.style.textDecoration = "none";
 document.querySelector("ol").appendChild(email);
 
+const hrMiddle = document.createElement("hr");
+hrMiddle.style.background = "#b06969";
+hrMiddle.style.width = "100%";
+hrMiddle.style.margin = "15px";
+hrMiddle.style.height = "10px";
+document.querySelector("ol").appendChild(hrMiddle);
+
 const address = document.createElement("span");
 address.textContent =
   "address : No15a Jebba-Street, 123 East Street lagos, Nigeria";
 address.style.textDecoration = "none";
 document.querySelector("ol").appendChild(address);
+
+const hrBottom = document.createElement("hr");
+hrBottom.style.background = "#b06969";
+hrBottom.style.margin = "15px";
+hrBottom.style.width = "100%";
+hrBottom.style.height = "10px";
+document.querySelector("ol").appendChild(hrBottom);
