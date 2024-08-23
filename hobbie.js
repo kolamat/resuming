d14b64fb5096db510c794d357f96b63ff9615161
kolamat <matanmidupe004@gmail.com>
2024-08-23 10:57:43 +0100

    On branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   Hobbies.html
    	modified:   contact.html
    	modified:   hobbie.js

--- hobbie.js
+++ hobbie.js
@@ -2,3 +2,6 @@
 const hobbies = document.getElementById("hobbies");
 hobbies.style.display = "grid";
 hobbies.style.justifyContent = "center";
+hobbies.style.width = "100%";
+hobbies.style.fontSize = "30px";
+hobbies.style.alignItems = "center";
