90818d1e2b29c90db5e646bcc11492fd051a2f7b
kolamat <matanmidupe004@gmail.com>
2024-08-23 11:04:16 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   contact.js
    	modified:   hobbie.js

--- contact.js
+++ contact.js
@@ -2,3 +2,7 @@
 const contact = document.getElementById("contact");
 contact.style.display = "grid";
 contact.style.justifyContent = "center";
+contact.style.width = "100%";
+contact.style.fontSize = "32px";
+contact.style.fontWeight = "bold";
+contact.style.placeItems = "center";
