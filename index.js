e159177d6a2e9fd05f76f9d63f6fc64016e52e71
kolamat <matanmidupe004@gmail.com>
2024-08-23 09:22:20 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.html
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const primary = document.createElement("p")
-  primary.textContent = "Ago-Egba Nursery and Primary School"
-document.getElementById("education").appendChild(primary)
+const primary = document.createElement("li");
+primary.textContent = "Ago-Egba Nursery and Primary School";
+document.getElementById("educations").appendChild(primary);
 
-const juniorSecondary = "Denton Junior Grammar School"
-  juniorSecondary.textContent = "Ago-Egba Nursery and Primary School"
-document.getElementById("education").appendChild(juniorSecondary);
+const juniorSecondary = document.createElement("li");
+juniorSecondary.textContent = "Denton Junior Grammar School";
+document.getElementById("educations").appendChild(juniorSecondary);
 
-const seniorSecondary = "Lagos-City Senior Secondary School"
-  seniorSecondary.textContent = "Ago-Egba Nursery and Primary School"
-document.getElementById("education").appendChild(seniorSecondary);
+const hr = document.createElement("hr");
+document.getElementById("educations").appendChild(hr);
+
+const seniorSecondary = document.createElement("li");
+seniorSecondary.textContent = "Lagos-City Senior Secondary School";
+document.getElementById("educations").appendChild(seniorSecondary);
+
+const higherEduation = document.createElement("li");
+higherEduation.textContent =
+  "Ordinary national diplomal (OND) 1, computer Engineer - Yaba College of Technology (2023-2025)";
+document.getElementById("educations").appendChild(higherEduation);
