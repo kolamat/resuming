6730c59a08caac436bc8a46b3e22eb40d14c14f6
kolamat <matanmidupe004@gmail.com>
2024-08-23 11:08:58 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   contact.html
    	modified:   contact.js

--- contact.js
+++ contact.js
@@ -3,6 +3,8 @@ const contact = document.getElementById("contact");
 contact.style.display = "grid";
 contact.style.justifyContent = "center";
 contact.style.width = "100%";
-contact.style.fontSize = "32px";
+contact.style.fontSize = "29px";
 contact.style.fontWeight = "bold";
 contact.style.placeItems = "center";
+
+const paraGraph = document.querySelectorAll("p");
