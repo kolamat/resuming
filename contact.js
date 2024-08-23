9b3e8031ae166a9a2abe0d0ed681009d35b0718d
kolamat <matanmidupe004@gmail.com>
2024-08-23 11:43:46 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   contact.js

--- contact.js
+++ contact.js
@@ -8,6 +8,7 @@ contact.style.fontWeight = "bold";
 contact.style.placeItems = "center";
 
 const whatsappNumber = document.createElement("a");
-whatsappNumber.textContent = "Whatsapp(2348150672809)";
+whatsappNumber.textContent = "Number : Whatsapp(2348150672809)";
 whatsappNumber.href = "https://wa.me/+2348150672809";
+whatsappNumber.style.textDecoration = "none";
 document.querySelector("ol").appendChild(whatsappNumber);
