de005e8d74cf675762a3d812059b7613d1482231
kolamat <matanmidupe004@gmail.com>
2024-08-23 11:53:31 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   contact.js

--- contact.js
+++ contact.js
@@ -7,8 +7,24 @@ contact.style.fontSize = "33px";
 contact.style.fontWeight = "bold";
 contact.style.placeItems = "center";
 
+const orderedList = document.querySelector("ol");
+orderedList.style.gap = "10px";
+orderedList.style.display = "grid";
+
 const whatsappNumber = document.createElement("a");
 whatsappNumber.textContent = "Number : Whatsapp(2348150672809)";
 whatsappNumber.href = "https://wa.me/+2348150672809";
 whatsappNumber.style.textDecoration = "none";
 document.querySelector("ol").appendChild(whatsappNumber);
+
+const email = document.createElement("a");
+email.textContent = "Email : Matanmidupe004@gmail.com";
+email.href = "https://matanmidupe004@gmail.com";
+email.style.textDecoration = "none";
+document.querySelector("ol").appendChild(email);
+
+const address = document.createElement("span");
+address.textContent =
+  "address : No15a Jebba-Street, 123 East Street lagos, Nigeria";
+address.style.textDecoration = "none";
+document.querySelector("ol").appendChild(address);
