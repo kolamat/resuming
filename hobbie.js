a1465eb0dbf52a183f765404b866524052cb26e6
kolamat <matanmidupe004@gmail.com>
2024-08-24 03:01:30 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   hobbie.js

--- hobbie.js
+++ hobbie.js
@@ -18,48 +18,52 @@ orderedLst.style.margin = "16px 0px";
 document.getElementById("hobbies").appendChild(orderedLst);
 
 const list1 = document.createElement("li");
+list1.id = "list1";
 list1.textContent = "I love Playing Video Games 🎮.";
 document.getElementById("hobbiesItems").appendChild(list1);
 
-const hr = document.createElement("hr");
-hr.style.background = "#b06969";
-hr.style.width = "100%";
-hr.style.margin = "15px 0px 0px 0px";
-hr.style.height = "6px";
-document.getElementById("mobile").appendChild(hr);
+const hr1 = document.createElement("hr");
+hr1.style.background = "#b06969";
+hr1.style.width = "100%";
+hr1.style.margin = "15px 0px 0px 0px";
+hr1.style.height = "6px";
+document.getElementById("list1").appendChild(hr1);
 
 const list2 = document.createElement("li");
+list2.id = "list2";
 list2.textContent = "I love reading 📚.";
 document.getElementById("hobbiesItems").appendChild(list2);
 
-const hr = document.createElement("hr");
-hr.style.background = "#b06969";
-hr.style.width = "100%";
-hr.style.margin = "15px 0px 0px 0px";
-hr.style.height = "6px";
-document.getElementById("mobile").appendChild(hr);
+const hr2 = document.createElement("hr");
+hr2.style.background = "#b06969";
+hr2.style.width = "100%";
+hr2.style.margin = "15px 0px 0px 0px";
+hr2.style.height = "6px";
+document.getElementById("list2").appendChild(hr2);
 
 const list3 = document.createElement("li");
 list3.textContent = "I love listening to Musics🎶.";
 document.getElementById("hobbiesItems").appendChild(list3);
 
-const hr = document.createElement("hr");
-hr.style.background = "#b06969";
-hr.style.width = "100%";
-hr.style.margin = "15px 0px 0px 0px";
-hr.style.height = "6px";
-document.getElementById("mobile").appendChild(hr);
+const hr3 = document.createElement("hr");
+list3.id = "list3";
+hr3.style.background = "#b06969";
+hr3.style.width = "100%";
+hr3.style.margin = "15px 0px 0px 0px";
+hr3.style.height = "6px";
+document.getElementById("list3").appendChild(hr3);
 
 const list4 = document.createElement("li");
 list4.textContent = "I love listening to Advices 💬, and";
 document.getElementById("hobbiesItems").appendChild(list4);
 
-const hr = document.createElement("hr");
-hr.style.background = "#b06969";
-hr.style.width = "100%";
-hr.style.margin = "15px 0px 0px 0px";
-hr.style.height = "6px";
-document.getElementById("mobile").appendChild(hr);
+const hr4 = document.createElement("hr");
+list4.id = "list4";
+hr4.style.background = "#b06969";
+hr4.style.width = "100%";
+hr4.style.margin = "15px 0px 0px 0px";
+hr4.style.height = "6px";
+document.getElementById("list4").appendChild(hr4);
 
 const list5 = document.createElement("li");
 list5.textContent = "And most importantly, I love writing CODES 💻👨🏼‍💻";
