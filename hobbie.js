abd2c4bbf6c059bdfaa015220084f83abf10c3b9
kolamat <matanmidupe004@gmail.com>
2024-08-24 02:25:10 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   hobbie.js

--- hobbie.js
+++ hobbie.js
@@ -7,10 +7,12 @@ document.getElementById("hobbies").appendChild(h1);
 
 const hobbies = document.getElementById("hobbies");
 hobbies.style.display = "grid";
-hobbies.style.justifyContent = "center";
+hobbies.style.marginLeft = "auto";
+hobbies.style.marginRight = "auto";
+// hobbies.style.justifyContent = "center";
 hobbies.style.width = "100%";
-hobbies.style.fontSize = "32px";
-hobbies.style.fontWeight = "bold";
+hobbies.style.fontSize = "40px";
+hobbies.style.fontWeight = "normal";
 hobbies.style.placeItems = "center";
 
 const orderedLst = document.createElement("ol");
@@ -19,14 +21,14 @@ orderedLst.style.margin = "16px 0px";
 document.getElementById("hobbies").appendChild(orderedLst);
 
 const list1 = document.createElement("li");
-list1.textContent = "I love Playing Video Games.";
+list1.textContent = "I love Playing Video Games 🎮.";
 document.getElementById("hobbiesItems").appendChild(list1);
 const list2 = document.createElement("li");
-list2.textContent = "I love reading.";
+list2.textContent = "I love reading 📚.";
 document.getElementById("hobbiesItems").appendChild(list2);
 const list3 = document.createElement("li");
-list3.textContent = "I love listening to Advices, and";
+list3.textContent = "I love listening to Advices 💬, and";
 document.getElementById("hobbiesItems").appendChild(list3);
 const list4 = document.createElement("li");
-list4.textContent = "I love listening to Musics.";
+list4.textContent = "I love listening to Musics🎶.";
 document.getElementById("hobbiesItems").appendChild(list4);
